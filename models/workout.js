ae34e1334dfd6eebb5799f912ed9dfc901952fe7
Samwise <samuelfoster0@gmail.com>
2020-04-30 10:01:34 -0700

    Made some minor changes to models

--- models/workout.js
+++ models/workout.js
@@ -12,10 +12,44 @@ const WorkoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now,
-    
   },
 
-  exercises: [{}]
+  exercises: [{
+
+    type: {
+      type: String,
+      trim: true,
+      required: "Enter a exercise type"
+    },
+
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter a exercise type"
+
+    },
+
+    duration: {
+      type: Number,
+    },
+
+    weight: {
+      type: Number,
+    },
+
+    reps: {
+      type: Number,
+    },
+
+    sets: {
+      type: Number,
+    },
+
+    distance: {
+      type: Number,
+    },
+    
+  }]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);

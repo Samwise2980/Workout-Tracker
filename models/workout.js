e73982918a5001bed8cdd071ed6055bc54a5caf2
Samwise <samuelfoster0@gmail.com>
2020-04-29 18:36:56 -0700

    got the post update route to push to the exercises array

--- models/workout.js
+++ models/workout.js
@@ -6,41 +6,42 @@ const WorkoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now,
+    
   },
 
   exercises: [
     {
-      type: {
-        type: String,
-        trim: true,
-        required: "Type is required",
-      },
+      // type: {
+      //   type: String,
+      //   trim: true,
+      //   required: "Type is required",
+      // },
 
-      name: {
-        type: String,
-        trim: true,
-        required: "Name is required",
-      },
+      // name: {
+      //   type: String,
+      //   trim: true,
+      //   required: "Name is required",
+      // },
 
-      duration: {
-        type: Number,
-        required: "Number is required",
-      },
+      // duration: {
+      //   type: Number,
+      //   required: "Number is required",
+      // },
 
-      weight: {
-        type: Number,
-        required: "Weight is required",
-      },
+      // weight: {
+      //   type: Number,
+      //   required: "Weight is required",
+      // },
 
-      reps: {
-        type: Number,
-        required: "Reps is required",
-      },
+      // reps: {
+      //   type: Number,
+      //   required: "Reps is required",
+      // },
 
-      sets: {
-        type: Number,
-        required: "Sets is required",
-      },
+      // sets: {
+      //   type: Number,
+      //   required: "Sets is required",
+      // },
     }
   ]
 });
--- routes/api.js
+++ routes/api.js
@@ -7,28 +7,73 @@ const db = require("../models");
 
 // Creates a workout using data in the request body.
 router.post("/api/workouts", (req, res) => {
-  // CODE HERE
+  console.log(req.body);
+  db.Workout.create(req.body)
+    .then(() => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 // Respond with workout for id url parameter. This should
 // respond with the updated workout json
 router.put("/api/workouts/:id", (req, res) => {
-  // CODE HERE
+  console.log(req.body);
+  db.Workout.update(
+    {
+      _id: req.params.id,
+    },
+    {
+      $push: { exercises: req.body }
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
 });
 
 // Respond with json for all the workouts in an array.
 router.get("/api/workouts", (req, res) => {
-  // CODE HERE
+  db.Workout.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 // Respond with json array containing the last 7 workouts
 router.get("/api/workouts/range", (req, res) => {
-  // CODE HERE
+  db.Workout.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
+  }).sort({ day: 1 }).limit(7);
 });
 
 // Delete workout with id matching id in the request body.
 router.delete("/api/workouts", (req, res) => {
-  // CODE HERE
+  db.Workout.remove(
+    {
+      _id: req.body.id,
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
 });
 
 module.exports = router;
